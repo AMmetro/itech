e5c2f91663b7ebd96785c57310c3b362b9cd9b78
AMmetro <metro.trend.now@gmail.com>
2021-09-23 20:26:17 +0300

    formating code

--- js/modal.js
+++ js/modal.js
@@ -1,80 +1,80 @@
 document.getElementById("btnOpenModal1").onclick = function () {
-  var message="This window is instanse from Prototype ModalWindow"
-  var modalWindow = new ModalWindow(message);
-  modalWindow.makeModal();
-  modalWindow.addContent();
-  modalWindow.addCloseBtn();
+    var message = "This window is instanse from Prototype ModalWindow"
+    var modalWindow = new ModalWindow(message);
+    modalWindow.makeModal();
+    modalWindow.addContent();
+    modalWindow.addCloseBtn();
 }
 
 document.getElementById("btnOpenModal2").onclick = function () {
-  var message="This window is inheritor from instance of PrototypeModalWindow"
-  var modifayedModalWidow = new ModifayedModalWidow(message)
-  modifayedModalWidow.makeModal();
-  modifayedModalWidow.addContent();
-  modifayedModalWidow.addCloseBtn();
-  modifayedModalWidow.addInput();
+    var message = "This window is inheritor from instance of PrototypeModalWindow"
+    var modifayedModalWidow = new ModifayedModalWidow(message)
+    modifayedModalWidow.makeModal();
+    modifayedModalWidow.addContent();
+    modifayedModalWidow.addCloseBtn();
+    modifayedModalWidow.addInput();
 }
 
-//-------------------------------------------------------------------- 
+//-------------------------------------------------------------------
 
 var ModalWindow = function (message) {
-  this.message=message;
- }
+    this.message = message;
+}
 
-  ModalWindow.prototype.makeModal = function () {
-      modal = document.createElement("div");
-      modal.classList.add("modal")
-      modal.style.display = "block";
-      document.querySelector('body').appendChild(modal);
+ModalWindow.prototype.makeModal = function () {
+    modal = document.createElement("div");
+    modal.classList.add("modal")
+    modal.style.display = "block";
+    document.querySelector('body').appendChild(modal);
+}
+
+ModalWindow.prototype.addContent = function () {
+    var modalContentHolder = document.createElement("div");
+    modalContentHolder.classList.add("modal-content");
+    var modalCloseBtn = document.createElement("span")
+    modalCloseBtn.classList.add("modalCloseBtn");
+    modalCloseBtn.innerHTML = "&times";
+    modalCloseBtn.onclick = function () {
+        modal.style.display = "none";
+        modalContentHolder.remove();
+    };
+
+    modalContentHolder.appendChild(modalCloseBtn);
+    var modalContentTxt = document.createElement("p");
+    modalContentTxt.innerText = this.message;
+    modalContentHolder.appendChild(modalContentTxt);
+    modal.appendChild(modalContentHolder);
+
+    this.addCloseBtn = function () {
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                modalContentHolder.remove()
+            }
+        }
     }
-
-  ModalWindow.prototype.addContent = function () {
-        var modalContentHolder = document.createElement("div");
-        modalContentHolder.classList.add("modal-content");
-        var modalCloseBtn = document.createElement("span")
-        modalCloseBtn.classList.add("modalCloseBtn");
-        modalCloseBtn.innerHTML = "&times";
-        modalCloseBtn.onclick = function () {
-            modal.style.display = "none";
-            modalContentHolder.remove();
-        };
-
-          modalContentHolder.appendChild(modalCloseBtn);
-          var modalContentTxt = document.createElement("p");
-          modalContentTxt.innerText = this.message;
-          modalContentHolder.appendChild(modalContentTxt);
-          modal.appendChild(modalContentHolder);
-
-          this.addCloseBtn = function () {
-              window.onclick = function (event) {
-                  if (event.target == modal) {
-                      modal.style.display = "none";
-                      modalContentHolder.remove()
-                  }
-              }
-          }
 }
 
 var ModifayedModalWidow = function (options) {
-  ModalWindow.apply(this, arguments);
-} 
+    ModalWindow.apply(this, arguments);
+}
 
-//  reassign prototype for ModifayedModalWidow with Object.create(parametrs) 
+//  reassign prototype for ModifayedModalWidow with Object.create(parametrs)
 ModifayedModalWidow.prototype = Object.create(ModalWindow.prototype);
 //specifically constructor
 ModifayedModalWidow.prototype.constructor = ModifayedModalWidow;
 
 ModifayedModalWidow.prototype.addInput = function () {
-  this.type = "this ModifayedModalWidow is inheritance of PrototypeModalWindow";
-  var inputHolder = document.createElement("div");
-  inputHolder.classList.add("inputHolder");
-  var input = document.createElement("Input");
-  input.classList.add("input");
-  var hint = document.createElement("p");
-  hint.classList.add("hint");
-  hint.innerText = "put some info";
+    this.type = "this ModifayedModalWidow is inheritance of PrototypeModalWindow";
+    var inputHolder = document.createElement("div");
+    inputHolder.classList.add("inputHolder");
+    var input = document.createElement("Input");
+    input.classList.add("input");
+    var hint = document.createElement("p");
+    hint.classList.add("hint");
+    hint.innerText = "put some info";
 
-  inputHolder.appendChild(hint);
-  inputHolder.appendChild(input);
-  modal.appendChild(inputHolder);
-} 
+    inputHolder.appendChild(hint);
+    inputHolder.appendChild(input);
+    modal.appendChild(inputHolder);
+}
--- js/toast.js
+++ js/toast.js
@@ -1,97 +1,96 @@
-    document.querySelector ('.button_pure').onclick=function () {
-    toastPopup=new PurePopup()
+document.querySelector('.button_pure').onclick = function () {
+    toastPopup = new PurePopup()
     toastPopup.makeToast('I am pure Popup');
-      };
+};
 
-    document.querySelector ('.button_styled').onclick=function () {
-    var styledPopup=new StyledPopup()
+document.querySelector('.button_styled').onclick = function () {
+    var styledPopup = new StyledPopup()
     styledPopup.makeToast('I am styled Popup', 3000);
-    styledPopup.addStyle('toast-skin-styled','&hellip;');
-    };
+    styledPopup.addStyle('toast-skin-styled', '&hellip;');
+};
 
-    document.querySelector ('.button_error').onclick=function () {
-      var toastPopupError=new ToastPopupError()
-      toastPopupError.makeToast('Error!!!Oops, somethink is wrong', 2000);
-      toastPopupError.addStyle('toast-skin-error','&#9940;');
-      };
+document.querySelector('.button_error').onclick = function () {
+    var toastPopupError = new ToastPopupError()
+    toastPopupError.makeToast('Error!!!Oops, somethink is wrong', 2000);
+    toastPopupError.addStyle('toast-skin-error', '&#9940;');
+};
 
-     document.querySelector ('.button_warning').onclick=function () {
-     var toastPopupWarning=new ToastPopupWarning();
-     toastPopupWarning.makeToast('Warning!!! Pay atention', 3000);
-     toastPopupWarning.addStyle('toast-skin-warning','&#128293;');
-    };
+document.querySelector('.button_warning').onclick = function () {
+    var toastPopupWarning = new ToastPopupWarning();
+    toastPopupWarning.makeToast('Warning!!! Pay atention', 3000);
+    toastPopupWarning.addStyle('toast-skin-warning', '&#128293;');
+};
 
-    document.querySelector ('.button_success').onclick=function () {
-      var toastPopupSuccess=new ToastPopupSuccess()
-      toastPopupSuccess.makeToast('Succes!!! Everything is fine', 2000);
-      toastPopupSuccess.addStyle('toast-skin-success','&#9989;');
-      };
+document.querySelector('.button_success').onclick = function () {
+    var toastPopupSuccess = new ToastPopupSuccess()
+    toastPopupSuccess.makeToast('Succes!!! Everything is fine', 2000);
+    toastPopupSuccess.addStyle('toast-skin-success', '&#9989;');
+};
 
-      document.querySelector ('.button_info').onclick=function () {
-        var toastPopupInfo = new ToastPopupInfo();
-        toastPopupInfo.makeToast('Info!!! Read please.', 6000);
-        toastPopupInfo.addStyle('toast-skin-info','&#128276;');
-      }
+document.querySelector('.button_info').onclick = function () {
+    var toastPopupInfo = new ToastPopupInfo();
+    toastPopupInfo.makeToast('Info!!! Read please.', 6000);
+    toastPopupInfo.addStyle('toast-skin-info', '&#128276;');
+}
 
 
- //-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
-      var PurePopup = function (message) {
-      this.message=message;
-      this.toastMessage = document.createElement ('div');
-      this.closeButton = document.createElement ('div');
-      this.toastItem = document.createElement('div');
-      this.icon = document.createElement ('div');
-    }
+var PurePopup = function (message) {
+    this.message = message;
+    this.toastMessage = document.createElement('div');
+    this.closeButton = document.createElement('div');
+    this.toastItem = document.createElement('div');
+    this.icon = document.createElement('div');
+}
 
-    PurePopup.prototype.makeToast = function (message,timeRemove=5000) {
-       this.closeButton.classList.add('close-button');
-      this.closeButton.addEventListener('click',()=>{
-                           this.toastItem.remove()
-                      })
-      document.querySelector('body').appendChild(this.closeButton);
+PurePopup.prototype.makeToast = function (message, timeRemove = 5000) {
+    this.closeButton.classList.add('close-button');
+    this.closeButton.addEventListener('click', () => {
+        this.toastItem.remove()
+    })
+    document.querySelector('body').appendChild(this.closeButton);
 
-      var closeButtonContainer = document.createElement ('div');
-      closeButtonContainer.classList.add('close_button-container');
-      closeButtonContainer.appendChild(this.closeButton);
+    var closeButtonContainer = document.createElement('div');
+    closeButtonContainer.classList.add('close_button-container');
+    closeButtonContainer.appendChild(this.closeButton);
 
-      this.toastMessage.classList.add ('toastMessage');
-      this.toastMessage.innerHTML = message;
+    this.toastMessage.classList.add('toastMessage');
+    this.toastMessage.innerHTML = message;
 
-      this.toastItem.classList.add('toastItem');
-      this.toastItem.appendChild (closeButtonContainer);
-      this.toastItem.appendChild (this.toastMessage);
+    this.toastItem.classList.add('toastItem');
+    this.toastItem.appendChild(closeButtonContainer);
+    this.toastItem.appendChild(this.toastMessage);
 
-      var toastField = document.querySelector ('.toastField');
-        if (toastField) {
-           toastField.appendChild (this.toastItem);
-              }
-        else {
+    var toastField = document.querySelector('.toastField');
+    if (toastField) {
+        toastField.appendChild(this.toastItem);
+    } else {
         let toastField = document.createElement('div');
         toastField.classList.add('toastField');
-        toastField.appendChild (this.toastItem);
+        toastField.appendChild(this.toastItem);
         document.querySelector('body').appendChild(toastField);
-        }
+    }
 
-        var self = this;
-        setTimeout (function (){
-          self.toastItem.remove();
-          var allToast = document.querySelectorAll('.toastItem');
-          if (allToast.length == 0) document.querySelector('.toastField').remove();
-        }, timeRemove);
-   }
+    var self = this;
+    setTimeout(function () {
+        self.toastItem.remove();
+        var allToast = document.querySelectorAll('.toastItem');
+        if (allToast.length == 0) document.querySelector('.toastField').remove();
+    }, timeRemove);
+}
 
 // ---------------------StyledPopup-------------------------
 
-        var StyledPopup = function (options) {
-        PurePopup.apply(this, arguments);
-        this.icon = document.createElement ('div');
-        this.addStyle = function(toastStyle, iconType) {
+var StyledPopup = function (options) {
+    PurePopup.apply(this, arguments);
+    this.icon = document.createElement('div');
+    this.addStyle = function (toastStyle, iconType) {
         this.toastItem.classList.add(toastStyle);
-        this.icon.innerHTML=iconType;
-        this.toastMessage.classList.add ('toastMessage');
+        this.icon.innerHTML = iconType;
+        this.toastMessage.classList.add('toastMessage');
         this.toastMessage.prepend(this.icon);
-      };
+    };
 }
 
 StyledPopup.prototype = Object.create(PurePopup.prototype);
@@ -100,7 +99,7 @@ StyledPopup.prototype.constructor = PurePopup;
 // --------------------ToastPopupError--------------------------
 
 var ToastPopupError = function (options) {
-  StyledPopup.apply(this, arguments);
+    StyledPopup.apply(this, arguments);
 }
 ToastPopupError.prototype = Object.create(StyledPopup.prototype);
 ToastPopupError.prototype.constructor = StyledPopup;
@@ -108,7 +107,7 @@ ToastPopupError.prototype.constructor = StyledPopup;
 // --------------------ToastPopupWarning--------------------------
 
 var ToastPopupWarning = function (options) {
-  StyledPopup.apply(this, arguments);
+    StyledPopup.apply(this, arguments);
 }
 ToastPopupWarning.prototype = Object.create(StyledPopup.prototype);
 ToastPopupWarning.prototype.constructor = StyledPopup;
@@ -116,7 +115,7 @@ ToastPopupWarning.prototype.constructor = StyledPopup;
 // --------------------ToastPopupSuccess--------------------------
 
 var ToastPopupSuccess = function (options) {
-  StyledPopup.apply(this, arguments);
+    StyledPopup.apply(this, arguments);
 }
 ToastPopupSuccess.prototype = Object.create(StyledPopup.prototype);
 ToastPopupSuccess.prototype.constructor = StyledPopup;
@@ -125,12 +124,12 @@ ToastPopupSuccess.prototype.constructor = StyledPopup;
 // --------------------ToastPopupInfo--------------------------
 
 var ToastPopupInfo = function (options) {
-  StyledPopup.apply(this, arguments);
+    StyledPopup.apply(this, arguments);
 }
 ToastPopupInfo.prototype = Object.create(StyledPopup.prototype);
 ToastPopupInfo.prototype.constructor = StyledPopup;
 
-// ------------------------------------------------------------
+
 
 
 
