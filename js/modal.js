3551fc7968b87b536d4758a5531264379cd036bd
Andrei <58401242+AMmetro@users.noreply.github.com>
2021-09-27 01:24:49 +0300

    update div implementing

--- js/modal.js
+++ js/modal.js
@@ -1,80 +1,74 @@
 document.getElementById("btnOpenModal1").onclick = function () {
-  var message="This window is instanse from Prototype ModalWindow"
-  var modalWindow = new ModalWindow(message);
+  const modalWindow = new ModalWindow("first message");
   modalWindow.makeModal();
-  modalWindow.addContent();
-  modalWindow.addCloseBtn();
 }
 
 document.getElementById("btnOpenModal2").onclick = function () {
-  var message="This window is inheritor from instance of PrototypeModalWindow"
-  var modifayedModalWidow = new ModifayedModalWidow(message)
-  modifayedModalWidow.makeModal();
-  modifayedModalWidow.addContent();
-  modifayedModalWidow.addCloseBtn();
+  var modifayedModalWidow = new ModifayedModalWidow("second message")
   modifayedModalWidow.addInput();
 }
 
 //-------------------------------------------------------------------- 
 
-var ModalWindow = function (message) {
-  this.message=message;
- }
+class ModalWindow {
+  constructor(message) {
+    this.message = message;
+    this.modal = document.createElement("div");
+    this.modalContentHolder = document.createElement("div");
+  }
+  makeModal() {
+    this.modal.classList.add("modal")
+    this.modal.style.display = "block";
+    document.querySelector('body').appendChild(this.modal);
 
-  ModalWindow.prototype.makeModal = function () {
-      modal = document.createElement("div");
-      modal.classList.add("modal")
-      modal.style.display = "block";
-      document.querySelector('body').appendChild(modal);
+    this.modalContentHolder = document.createElement("div");
+    this.modalContentHolder.classList.add("modal-content");
+    const modalCloseBtn = document.createElement("span")
+    modalCloseBtn.classList.add("modalCloseBtn");
+    modalCloseBtn.innerHTML = "&times";
+
+    var self = this;
+    modalCloseBtn.onclick = function () {
+      self.modal.style.display = "none";
+      self.modalContentHolder.remove();
+    };
+
+    this.modalContentHolder.appendChild(modalCloseBtn);
+    const modalContentTxt = document.createElement("p");
+    modalContentTxt.innerText = this.message;
+    this.modalContentHolder.appendChild(modalContentTxt);
+    this.modal.appendChild(this.modalContentHolder);
+
+
+    var self = this;
+    this.addCloseBtn = function () {
+      window.onclick = function (event) {
+        if (event.target == self.modal) {
+          self.modal.style.display = "none";
+          self.modalContentHolder.remove()
+        }
+      }
     }
-
-  ModalWindow.prototype.addContent = function () {
-        var modalContentHolder = document.createElement("div");
-        modalContentHolder.classList.add("modal-content");
-        var modalCloseBtn = document.createElement("span")
-        modalCloseBtn.classList.add("modalCloseBtn");
-        modalCloseBtn.innerHTML = "&times";
-        modalCloseBtn.onclick = function () {
-            modal.style.display = "none";
-            modalContentHolder.remove();
-        };
-
-          modalContentHolder.appendChild(modalCloseBtn);
-          var modalContentTxt = document.createElement("p");
-          modalContentTxt.innerText = this.message;
-          modalContentHolder.appendChild(modalContentTxt);
-          modal.appendChild(modalContentHolder);
-
-          this.addCloseBtn = function () {
-              window.onclick = function (event) {
-                  if (event.target == modal) {
-                      modal.style.display = "none";
-                      modalContentHolder.remove()
-                  }
-              }
-          }
+    this.addCloseBtn();
+  }
 }
 
-var ModifayedModalWidow = function (options) {
-  ModalWindow.apply(this, arguments);
-} 
+class ModifayedModalWidow extends ModalWindow {
+  constructor(options) {
+    super(options)
+  }
+  addInput(options) {
+    super.makeModal();
+    const inputHolder = document.createElement("div");
+    inputHolder.classList.add("inputHolder");
+    const input = document.createElement("Input");
+    input.classList.add("input");
+    const hint = document.createElement("p");
+    hint.classList.add("hint");
+    hint.innerText = "put some info";
 
-//  reassign prototype for ModifayedModalWidow with Object.create(parametrs) 
-ModifayedModalWidow.prototype = Object.create(ModalWindow.prototype);
-//specifically constructor
-ModifayedModalWidow.prototype.constructor = ModifayedModalWidow;
-
-ModifayedModalWidow.prototype.addInput = function () {
-  this.type = "this ModifayedModalWidow is inheritance of PrototypeModalWindow";
-  var inputHolder = document.createElement("div");
-  inputHolder.classList.add("inputHolder");
-  var input = document.createElement("Input");
-  input.classList.add("input");
-  var hint = document.createElement("p");
-  hint.classList.add("hint");
-  hint.innerText = "put some info";
-
-  inputHolder.appendChild(hint);
-  inputHolder.appendChild(input);
-  modal.appendChild(inputHolder);
-} 
+    inputHolder.appendChild(hint);
+    inputHolder.appendChild(input);
+    this.modalContentHolder.appendChild(inputHolder);
+  }
+}
