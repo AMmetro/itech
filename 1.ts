ee85761dae52f63f7633001d8fcb3082cbe172a9
AMmetro <metro.trend.now@gmail.com>
2021-09-29 13:24:52 +0300

    upload all task

---
+++ 1.ts
@@ -0,0 +1,18 @@
+// В функцию приходит массив состояний заказа и фильтруется
+// Нужно заменить FIXME на тип который вычисляется на освове OrderState
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type filteredStates='initial' | 'inWork' | 'fullfilled';
+type FIXME = filteredStates[];
+
+type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
+
+export const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
+  const filteredStates = [] as FIXME
+  orderStates.forEach((element) => {
+    if (element !== 'buyingSupplies' && element !== 'producing') {
+      filteredStates.push(element)
+    }
+  })
+  return filteredStates
+}
---
+++ 5.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FIXME = OrderState[]
+
+type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
+
+// Hint: with type guards return type can be removed
+export const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
+  return orderStates.filter((state) => state !== 'buyingSupplies' && state !== 'producing')
+}
