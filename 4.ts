09b950dc2f2edaec3c29e3aeb267bc8e92299991
AMmetro <metro.trend.now@gmail.com>
2021-09-30 22:51:18 +0300

    refactor task 4

--- 4.ts
+++ 4.ts
@@ -5,48 +5,41 @@
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
-type FIXME  ={
+type FIXME = SmolOrdeer | null
+
+type SmolOrdeer = {
   state: 'initial'
   sum: number
-}|{
+} | {
   state: 'inWork'
   sum: number
   workerId: number
-} | null
-
+}
 
 
 type Order =
+  SmolOrdeer
   | {
-      state: 'initial'
-      sum: number
-    }
-  | {
-    state: 'inWork'
+    state: 'buyingSupplies'
     sum: number
     workerId: number
+    suppliesSum: number
   }
   | {
-      state: 'buyingSupplies'
-      sum: number
-      workerId: number
-      suppliesSum: number
-    }
+    state: 'producing'
+    sum: number
+    workerId: number
+    suppliesSum: number
+    produceEstimate: Date
+  }
   | {
-      state: 'producing'
-      sum: number
-      workerId: number
-      suppliesSum: number
-      produceEstimate: Date
-    }
-  | {
-      state: 'fullfilled'
-      sum: number
-      workerId: number
-      suppliesSum: number
-      produceEstimate: Date
-      fullfillmentDate: Date
-    }
+    state: 'fullfilled'
+    sum: number
+    workerId: number
+    suppliesSum: number
+    produceEstimate: Date
+    fullfillmentDate: Date
+  }
 
 
 export const filterOnlyInitialAndInWorkOrder = (order: Order): FIXME => {
