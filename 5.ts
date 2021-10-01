0d5ce412aa537841dc3870a8ae5941f0dd1fa6e9
AMmetro <metro.trend.now@gmail.com>
2021-10-01 21:25:13 +0300

    add space

--- 5.ts
+++ 5.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
+
 type FIXME = OrderState[]
 
 type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
