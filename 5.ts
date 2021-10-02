dcac7a3dce0d656335119d9ac22d847efbf2b48b
AMmetro <metro.trend.now@gmail.com>
2021-10-02 19:09:53 +0300

    change task to filtration array

--- 5.ts
+++ 5.ts
@@ -3,11 +3,12 @@
 
 type FIXME = OrderState[]
 
+
 type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
 
+
 // Hint: with type guards return type can be removed
-export const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
+export const getUserOrderStates = (orderStates: OrderState[], excl:["buyingSupplies","producing"]): FIXME => {
 
-  return orderStates.filter((state) => state !== 'buyingSupplies' && state !== 'producing')
+  return orderStates.filter((state) => !excl.find(ele=>ele===state))
 }
-
