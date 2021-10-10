5d8868afedfaecec041b8141cc4d638bff35cfd7
AMmetro <metro.trend.now@gmail.com>
2021-10-10 19:52:46 +0300

    update task 4 and 5

--- 4.ts
+++ 4.ts
@@ -5,41 +5,40 @@
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
-type FIXME  = Omit<Order,'suppliesSum'|'produceEstimate'|'fullfillmentDate'>
-
-type SmolOrdeer = {
-  state: 'initial'
-  sum: number
-} | {
-  state: 'inWork'
-  sum: number
-  workerId: number
-}
-
+type FIXME = X<Order> | null
+type X<T> = T extends {state: 'initial' | 'inWork'} ? T : never
 
 type Order =
-  SmolOrdeer
+   | {
+      state: 'initial'
+      sum: number
+    }
   | {
-    state: 'buyingSupplies'
+    state: 'inWork'
     sum: number
     workerId: number
-    suppliesSum: number
   }
   | {
-    state: 'producing'
-    sum: number
-    workerId: number
-    suppliesSum: number
-    produceEstimate: Date
-  }
+      state: 'buyingSupplies'
+      sum: number
+      workerId: number
+      suppliesSum: number
+    }
   | {
-    state: 'fullfilled'
-    sum: number
-    workerId: number
-    suppliesSum: number
-    produceEstimate: Date
-    fullfillmentDate: Date
-  }
+      state: 'producing'
+      sum: number
+      workerId: number
+      suppliesSum: number
+      produceEstimate: Date
+    }
+  | {
+      state: 'fullfilled'
+      sum: number
+      workerId: number
+      suppliesSum: number
+      produceEstimate: Date
+      fullfillmentDate: Date
+    }
 
 
 export const filterOnlyInitialAndInWorkOrder = (order: Order): FIXME => {
--- 5.ts
+++ 5.ts
@@ -1,14 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
+// по аналогии с уроком сделал тайпгард, но как его применить не понимаю
+// это хоть в ту сторону размышления?
 
 type FIXME = OrderState[]
+    type Validanswer = 'initial' | 'inWork' | 'fullfilled'
+    type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
 
 
-type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
 
+    const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
 
-// Hint: with type guards return type can be removed
-export const getUserOrderStates = (orderStates: OrderState[], excl:["buyingSupplies","producing"]): FIXME => {
-
-  return orderStates.filter((state) => !excl.find(ele=>ele===state))
-}
+        const result = orderStates.filter((state) => {
+             
+            function isSatisfies(state:OrderState):state is Validanswer {
+                if (state =='initial') {return true}
+                if (state =='inWork') {return true}
+                if (state =='fullfilled') {return true}
+                else {return false}
+            };           
+              
+            function work(state:OrderState) {
+                if (isSatisfies(state)){return true}
+                else {return false}
+                                     }          
+              return work(state)
+        })
+        return result
+    }
